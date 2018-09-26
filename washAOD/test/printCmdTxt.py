705f24170424788cb76567e6e5968694b0d2edcc
Weinan Si <weinan.si@cern.ch>
2018-09-26 12:40:34 -0500

    a small tool to help print cmd

--- washAOD/test/printCmdTxt.py
+++ washAOD/test/printCmdTxt.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import os
 
 year = 2017
-frag = 'dsaMatcher'
+frag = 'jetMassSculpt'
+suffixTag = '100k' # [100k, Pythia, PythiaTest2]
 
 def main():
 
@@ -15,8 +17,11 @@ def main():
 
     ctaus = ['1p20e-03', '0p012', '0p12', '0p6', '1p2', '3p6']
     for c in ctaus:
-        print("nohup ./mkNtuple.sh SIDMmumu_Mps-200_MZp-1p2_ctau-{0}_100k {1} {2} &".format(
-            c, frag, year))
+        datalistF = 'SIDMmumu_Mps-200_MZp-1p2_ctau-{0}'.format(c)
+        if suffixTag:
+            datalistF = '_'.join([datalistF, suffixTag])
+        if not os.path.isfile('../data/{0}/{1}.list'.format(year, datalistF)): continue
+        print("nohup ./mkNtuple.sh {0} {1} {2} &".format(datalistF, frag, year))
 
 if __name__ == '__main__':
     main()
