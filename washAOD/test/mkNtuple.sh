8378613ca47f4a60a5b6e1ba8a791d9dfb1a22bb
Weinan Si <weinan.si@cern.ch>
2018-10-09 15:05:07 -0500

    redirect logs to inspect and rerun failed jobs

--- washAOD/test/mkNtuple.sh
+++ washAOD/test/mkNtuple.sh
@@ -3,7 +3,12 @@
 TAG=$1
 FRAG=$2
 YEAR=$3 #2017, 2018
+
 DATALIST=${TAG}.list
 OUTPUT=${FRAG}_${TAG}.root
+LOGDIR=Logs/${YEAR}
+LOGFILE=${FRAG}_${TAG}.log
 
-cmsRun ../cfg/${FRAG}_cfg.py year=${YEAR} inputFiles_load=../data/${YEAR}/${DATALIST} outputFile=${YEAR}/${OUTPUT} 2>&1
+[ -d ${LOGDIR} ] || mkdir -p ${LOGDIR}
+
+cmsRun ../cfg/${FRAG}_cfg.py year=${YEAR} inputFiles_load=../data/${YEAR}/${DATALIST} outputFile=${YEAR}/${OUTPUT} > ${LOGDIR}/${LOGFILE} 2>&1
--- washAOD/test/printCmdTxt.py
+++ washAOD/test/printCmdTxt.py
@@ -7,8 +7,36 @@ frag = 'tuplizer' #'jetTuplizer'
 suffixTag = '100k' # [100k, Pythia, PythiaTest2]
 prefixTag = 'SIDM_BsTo2DpTo4Mu'
 
+exited = '''SIDM_BsTo2DpTo4Mu_MBs-200_MDp-0p3_ctau-1p2_15k
+    SIDM_BsTo2DpTo4Mu_MBs-40_MDp-1p2_ctau-2p4_15k
+    SIDM_BsTo2DpTo4Mu_MBs-1000_MDp-0p3_ctau-0p24_15k
+    SIDM_BsTo2DpTo4Mu_MBs-1000_MDp-0p3_ctau-0p024_15k
+    SIDM_BsTo2DpTo4Mu_MBs-40_MDp-0p3_ctau-0p6_15k
+    SIDM_BsTo2DpTo4Mu_MBs-10_MDp-1p2_ctau-96_15k
+    SIDM_BsTo2DpTo4Mu_MBs-10_MDp-0p3_ctau-2p4_15k
+    SIDM_BsTo2DpTo4Mu_MBs-200_MDp-0p3_ctau-0p12_15k
+    SIDM_BsTo2DpTo4Mu_MBs-200_MDp-1p2_ctau-0p48_15k
+    SIDM_BsTo2DpTo4Mu_MBs-1000_MDp-1p2_ctau-0p96_15k'''
+
 def make_datalink_list(prefix):
     return [f for f in os.listdir('../data/'+year) if f.startswith(prefix)]
+
+def lookup_file(tag, filelist):
+    matched = [f for f in filelist if tag in f]
+    if len(matched):
+        return matched[0]
+    else:
+        return ''
+
+def lookup_files(tolookup, pool):
+    matched = []
+    for tl in tolookup:
+        result = lookup_file(tl, pool)
+        if result:
+            matched.append(result)
+        else:
+            print("No file found matched with: '"+tl+"'.")
+    return matched
 
 def main():
 
@@ -27,9 +55,18 @@ def main():
     #    if not os.path.isfile('../data/{0}/{1}.list'.format(year, datalistF)): continue
     #    print("nohup ./mkNtuple.sh {0} {1} {2} &".format(datalistF, frag, year))
 
-    for f in make_datalink_list(prefixTag):
+    allList = make_datalink_list(prefixTag)
+
+    exitedList = [e.strip() for e in exited.split('\n')]
+    datalinkFilelist = lookup_files(exitedList, allList)
+    for f in datalinkFilelist:
         datalistF = f.split('.')[0]
         print("nohup ./mkNtuple.sh {0} {1} {2} &".format(datalistF, frag, year))
+
+    print()
+    print('-'*79)
+    print('Total: ', len(datalinkFilelist), ' datalinks.')
+    print('-'*79)
 
 if __name__ == '__main__':
     main()
