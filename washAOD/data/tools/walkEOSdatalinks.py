f2332afbea98e774ebb0e52650edb717a6c59254
Weinan Si <weinan.si@cern.ch>
2018-10-15 14:43:11 -0500

    reflect true number of events

--- washAOD/data/tools/walkEOSdatalinks.py
+++ washAOD/data/tools/walkEOSdatalinks.py
@@ -6,7 +6,6 @@ from datetime import datetime
 BASEDIR = '/eos/uscms/store/user/wsi/MCSIDM'
 PREFIX  = 'SIDM_BsTo2DpTo4Mu'
 LINKFILEDIR = '../2018'
-TOTEVENTS = '15k'
 VERBOSE = True
 
 def sort_timestring(timestrlist):
@@ -23,10 +22,13 @@ if __name__ == '__main__':
 
         timestampdir = sort_timestring(os.listdir(os.path.join(BASEDIR, l)))[1]
         targetdir = os.path.join(BASEDIR, l, timestampdir)
-        linkfilename = l.replace('_slc6_amd64_gcc481_CMSSW_7_1_30_tarball', '') + '_' + TOTEVENTS + '.list'
-        cmd = "ls {0}/*_AOD.root |sed 's/^/file:/' > {1}/{2}".format(targetdir, LINKFILEDIR, linkfilename)
+        linkfilename = l.replace('_slc6_amd64_gcc481_CMSSW_7_1_30_tarball', '') + '_TOTEVENTS' + '.list'
+        cmd0 = "ls {0}/*_AOD.root |sed 's/^/file:/' > {1}/{2}".format(targetdir, LINKFILEDIR, linkfilename)
+        os.system(cmd0)
+
+        truelinkfilename = linkfilename.replace('TOTEVENTS', str(len(open(LINKFILEDIR+'/'+linkfilename).readlines()))+'k')
+        cmd1 = "mv {0}/{1} {0}/{2}".format(LINKFILEDIR, linkfilename, truelinkfilename)
+        os.system(cmd1)
 
         if VERBOSE:
-            print(targetdir+'/*_AOD.root', '  ==>  '+LINKFILEDIR+'/'+linkfilename, sep='\n')
-
-        os.system(cmd)
+            print(targetdir+'/*_AOD.root', '  ==>  '+LINKFILEDIR+'/'+truelinkfilename, sep='\n')
