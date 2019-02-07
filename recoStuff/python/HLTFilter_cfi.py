ad03cd460ae6681f14c3537860a8ef9ca1956dd7
Weinan Si <weinan.si@cern.ch>
2019-02-06 22:40:44 -0600

    adding

---
+++ recoStuff/python/HLTFilter_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+hltfilter = cms.EDFilter(
+    "HLTFilter",
+    TriggerEvent = cms.InputTag("hltTriggerSummaryAOD","","HLT"),
+    TriggerResults = cms.InputTag("TriggerResults","","HLT"),
+    TriggerPaths = cms.vstring(
+        'HLT_DoubleL2Mu23NoVtx_2Cha',
+        'HLT_DoubleL2Mu23NoVtx_2Cha_NoL2Matched',
+        'HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed',
+        'HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed_NoL2Matched'
+    ),
+    HltProcName = cms.string('HLT')
+)
