65cdbe24f4a76d89fe923000c5134f8701c456cc
Weinan Si <weinan.si@cern.ch>
2019-06-01 09:05:31 -0500

    update mva weights file

--- recoStuff/python/ffLeptonJetMVAEstimator_cfi.py
+++ recoStuff/python/ffLeptonJetMVAEstimator_cfi.py
@@ -4,6 +4,6 @@ ffLeptonJetMVAEstimatorParam = cms.PSet(
     estimatorName=cms.string("leptonJetXGB"),
     mvaTag=cms.string("default"),
     nCategories=cms.int32(1),
-    weightFileNames=cms.vstring("Firefighter/ffConfig/data/leptonJetXBG.xml"),
+    weightFileNames=cms.vstring("Firefighter/ffConfig/data/weights.xml.gz"),
     categoryCuts=cms.vstring("pt>=0."),
 )
