530018953ed504ecd79eea0d3dbb91ce8d13966d
Weinan Si <weinan.si@cern.ch>
2019-08-20 16:24:05 -0500

    discover dumper fragment was not tracked

---
+++ piedpiper/test/ffGENSIMContentDumper_cfg.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing("analysis")
+options.parseArguments()
+
+process = cms.Process("FFTEST")
+
+# import of standard configurations
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Generator_cff")
+process.load("GeneratorInterface.Core.genFilterSummary_cff")
+process.load("Configuration.StandardSequences.EndOfProcess_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+process.GlobalTag = GlobalTag(process.GlobalTag, "auto:mc", "")
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(options.maxEvents))
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames=cms.untracked.vstring(options.inputFiles),
+    duplicateCheckMode=cms.untracked.string("noDuplicateCheck"),
+)
+
+
+process.dummy2 = cms.EDAnalyzer("ffGENSIMContentDumper")
+
+
+# Path and EndPath definitions
+process.ana = cms.Path(process.dummy2)
+# Schedule definition
+process.schedule = cms.Schedule(process.ana)
