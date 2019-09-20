10b524d6dbc87b144b2f5717003b98e867b35ded
Weinan Si <weinan.si@cern.ch>
2019-09-20 15:16:00 -0500

    adjust qcd test sample file

--- ffConfig/python/test/initfftest.sh
+++ ffConfig/python/test/initfftest.sh
@@ -8,10 +8,11 @@ alias fftestsig4mu='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_signal-4m
 alias fftestsig2mu2e='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_signal-2mu2e.yml keepskim=1'
 alias fftestzz='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_ZZ.yml'
 alias fftestwww='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_WWW.yml'
-alias fftestqcd='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_QCDpT300To470.yml'
+alias fftestqcd='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_QCD.yml'
 alias fftestdy='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_DYJetsM50.yml'
 alias fftestdatac='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_DoubleMuon2018C.yml'
 alias fftestdatad='cmsRun $FFCFG config=$TESTCONFIGBASE/ffSuperConfig_DoubleMuon2018D.yml'
 
 alias ffv2testsig4mu='cmsRun $FFCFG2 config=$TESTCONFIGBASE/ffSuperConfig_signal-4mu.yml keepskim=1'
 alias ffv2testsig2mu2e='cmsRun $FFCFG2 config=$TESTCONFIGBASE/ffSuperConfig_signal-2mu2e.yml keepskim=1'
+alias ffv2testqcd='cmsRun $FFCFG2 config=$TESTCONFIGBASE/ffSuperConfig_QCD.yml'
